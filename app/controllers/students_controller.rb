70347b925a8e1de6aaf4b6c27b880f2ecbbeb893
cakejune <tyq444@gmail.com>
2022-12-14 11:24:22 -0500

    commit tree dd850e2357b7e690bf553ae71c8284a4672c6f20

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -5,4 +5,26 @@ class StudentsController < ApplicationController
     render json: students
   end
 
+  def grades
+    @students = Student.order(grade: :desc)
+    render json: students
+  end
+
+  def highest_grade
+    student = Student.order(grade: :desc).first
+    render json: student
+  end
+  
+  def show
+  student = Student.find(params[:id])
+  render json: student
+  end
+
+  def search
+  first_name = params[:first_name]
+  last_name = params[:last_name]
+  @students = Student.where(first_name: first_name).or(Student.where(last_name: last_name))
+    render json: @students
+  end
+
 end
